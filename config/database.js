e923533feb0c71508a253aae68ba6f30b59e907c
Mitrix05 <78508821+Mitrix05@users.noreply.github.com>
2025-09-22 18:05:14 +0200

    Teraz zadziała

--- config/database.js
+++ config/database.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
-    connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_URL')
-        },
-        debug: true,
-        pool: { min: 0, max: 7 },
-    }
+  connection: {
+    client: 'postgres',
+    connection: env('DATABASE_URL'),
+    debug: true,
+    pool: { min: 0, max: 7 },
+  },
 });
