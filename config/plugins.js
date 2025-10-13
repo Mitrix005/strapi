dce903f155ea7b30b1cb1291c1e232f54af4b8a4
Michal Szyszlo <mszyszlo@student.agh.edu.pl>
2025-10-13 19:54:25 +0200

    Odinstalowanie niektorych rzeczy

--- config/plugins.js
+++ config/plugins.js
@@ -5,65 +5,6 @@ module.exports = ({ env }) => ({
     },
   },
   'material-symbols': true,
-  prometheus: {
-    enabled: true,
-    config: {
-      collectDefaultMetrics: false, // or { prefix: 'my_app_' }
-
-      labels: {
-        app: "my-strapi-app",
-        environment: "production"
-      },
-
-      server: {
-        port: 9000,           // Metrics server port
-        host: '0.0.0.0',      // Metrics server host
-        path: '/metrics'      // Metrics endpoint path
-      },
-      normalize: [
-        [/\/(?:[a-z0-9]{24,25}|\d+)(?=\/|$)/, '/:id'], // Document IDs or numeric IDs
-        [/\/uploads\/[^\/]+\.[a-zA-Z0-9]+/, '/uploads/:file'], // Uploaded files with extensions
-      ]
-    }
-  },
-  'preview-button': {
-    config: {
-      contentTypes: [
-        {
-          uid: 'api::home.home',
-          published: {
-            url: 'http://localhost:3000',
-          },
-        },
-        {
-          uid: 'api::page.page',
-          draft: {
-            url: 'http://localhost:3000/api/preview',
-            query: {
-              type: 'page',
-              slug: '{slug}',
-            },
-          },
-          published: {
-            url: 'http://localhost:3000/{slug}',
-          },
-        },
-        {
-          uid: 'api::post.post',
-          draft: {
-            url: 'http://localhost:3000/api/preview',
-            query: {
-              type: 'post',
-              slug: '{slug}',
-            },
-          },
-          published: {
-            url: 'http://localhost:3000/blog/{slug}',
-          },
-        },
-      ],
-    },
-  },
   upload: {
     config: {
       provider: "local",
