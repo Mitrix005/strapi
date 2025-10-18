766fde14de0db11da031554910f9e8fc37ed8851
Mitrix05 <78508821+Mitrix05@users.noreply.github.com>
2025-10-18 13:07:35 +0200

    Logo change

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,28 +1,36 @@
 import logo from './extensions/logo.png'
 
-const config = {
-  locales: [
-    'pl'
-  ],
-  auth: {
-    logo: logo,
-  },
-  menu: {
-    logo: logo,
-  },
-  head: {
-    favicon: logo,
-  },
-
-  tutorials: false,
-  notifications: { releases: false },
-};
 
 const bootstrap = (app) => {
   console.log(app);
 };
 
 export default {
-  config,
+  config : {
+    locales: [
+      'pl'
+    ],
+    tutorials: false,
+    notifications: { releases: false },
+    head: {
+      favicon: logo,
+    },
+    auth: {
+      logo: logo,
+    },
+    menu: {
+      logo: logo,
+    },
+    translations: {
+      pl: {
+        "Auth.form.welcome.title": "Panel Administracyjny",
+        "Content Manager": "Menedżer treści",
+        "Content Type Builder": "Kreator typów treści",
+      },
+      en: {
+        "Auth.form.welcome.title": "Panel Administracyjny"
+      }
+    },
+  },
   bootstrap,
 };
